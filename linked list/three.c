c357b0b8632ae5d1c3801f9fc394650d187b0cb4
kharanshu <kharanshuw@gmail.com>
2024-04-07 23:06:38 +0530

    linked list

--- linked list/three.c
+++ linked list/three.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 struct mynode
 {
     int data;
-    struct mynode *ptr;
+    struct mynode *next;
 };
 struct mynode *head;
 
@@ -14,14 +14,16 @@ void display();
 void insert_at_end();
 int main()
 {
-    int choice=0;
+    int choice = 0;
 
     while (1)
     {
-        printf("enter choice \n 1.insert node at begining");
-        printf("\n2.display\n");
-        printf("3. exit ");
-        scanf("%d",&choice);
+        printf("\nenter choice \n 1.insert node at begining");
+        printf("\n2.display");
+        printf("\n3.exit ");
+        printf("\n4.insert_at_end\n");
+
+        scanf("%d", &choice);
         switch (choice)
         {
         case 1:
@@ -31,56 +33,88 @@ int main()
             display();
             break;
         case 3:
-           return 0;
+            return 0;
+            break;
+        case 4:
+            insert_at_end();
+            break;
         default:
             printf("\nwrong choice\n");
             break;
         }
     }
-    
+
     return 0;
 }
 
-void insert_at_begining(){
-    struct mynode *ptr;
+void insert_at_begining()
+{
+    struct mynode *next;
     int data;
     printf("enter no ");
-    scanf("%d",&data);
-    ptr=(struct mynode *)malloc(sizeof (struct mynode *));
-    if (ptr==NULL)
+    scanf("%d", &data);
+    next = (struct mynode *)malloc(sizeof(struct mynode *));
+    if (next == NULL)
     {
-       printf("\nptr is null memory not allocated\n");
+        printf("\nnext is null memory not allocated\n");
     }
     else
     {
-        ptr->data=data;
-        ptr->ptr=head;
-        head=ptr;
+        next->data = data;
+        next->next = head;
+        head = next;
         printf("\n node inserted \n");
     }
-    
 }
 
 void insert_at_end()
 {
-    
+    struct mynode *ptr, *temp;
+    int data;
+    ptr = (struct mynode *)malloc(sizeof(struct mynode *));
+    printf("\nenter value\t");
+    scanf("%d", &data);
+    if (ptr == NULL)
+    {
+        printf("overloded");
+    }
+    else
+    {
+        temp = head;
+        ptr->data = data;
+        if (head == NULL)
+        {
+            head = ptr;
+            ptr->next = NULL;
+            printf("\nnode inserted");
+        }
+        else
+        {
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = ptr;
+            ptr->next = NULL;
+            printf("\nnode added at last position");
+        }
+    }
 }
 
-void display(){
-    struct mynode *ptr;
-    ptr=head;
-    if (ptr==NULL)
+void display()
+{
+    struct mynode *next;
+    next = head;
+    if (next == NULL)
     {
         printf("\nnothing to print\n");
     }
     else
     {
-        while (ptr != NULL)
+        while (next != NULL)
         {
-            printf("\n %d \n",ptr->data);
-            ptr=ptr->ptr;
+            printf("\n %d \n", next->data);
+            next = next->next;
         }
-        
     }
-    
 }
