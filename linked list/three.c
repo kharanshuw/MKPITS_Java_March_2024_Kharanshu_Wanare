47035f7315b2e94909b680b1ddd4d35bb6b06a10
kharanshu <kharanshuw@gmail.com>
2024-04-07 22:48:54 +0530

    linked list

--- linked list/three.c
+++ linked list/three.c
@@ -11,9 +11,33 @@ void insert_at_begining();
 
 void display();
 
-
+void insert_at_end();
 int main()
 {
+    int choice=0;
+
+    while (1)
+    {
+        printf("enter choice \n 1.insert node at begining");
+        printf("\n2.display\n");
+        printf("3. exit ");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:
+            insert_at_begining();
+            break;
+        case 2:
+            display();
+            break;
+        case 3:
+           return 0;
+        default:
+            printf("\nwrong choice\n");
+            break;
+        }
+    }
+    
     return 0;
 }
 
@@ -25,14 +49,38 @@ void insert_at_begining(){
     ptr=(struct mynode *)malloc(sizeof (struct mynode *));
     if (ptr==NULL)
     {
-       printf("ptr is null memory not allocated");
+       printf("\nptr is null memory not allocated\n");
     }
     else
     {
         ptr->data=data;
         ptr->ptr=head;
         head=ptr;
-        printf("\n node inserted");
+        printf("\n node inserted \n");
+    }
+    
+}
+
+void insert_at_end()
+{
+    
+}
+
+void display(){
+    struct mynode *ptr;
+    ptr=head;
+    if (ptr==NULL)
+    {
+        printf("\nnothing to print\n");
+    }
+    else
+    {
+        while (ptr != NULL)
+        {
+            printf("\n %d \n",ptr->data);
+            ptr=ptr->ptr;
+        }
+        
     }
     
 }
