711515656c42cb58c9b041806abd0281e8f9f839
Kharanshu <kharanshuw@gmail>
2024-04-29 11:04:36 +0530

    assignment 9

--- Java/Assinment9/8.java
+++ Java/Assinment9/8.java
@@ -1,8 +1,50 @@
-package assignment9;
-
 public interface DatabaseOperations {
     public void create();
     public void read();
     public void update();
     public void delete();
+}
+public class MySQLDatabase implements DatabaseOperations{
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
+
+
+public class OracleDatabase implements DatabaseOperations{
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
 }
