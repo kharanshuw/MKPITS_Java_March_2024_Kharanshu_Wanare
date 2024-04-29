daf289b22dc8158587d36741955268d68aa8c671
Kharanshu <kharanshuw@gmail>
2024-04-29 11:55:45 +0530

    assignment9

---
+++ Java/Assinment9/2.java
@@ -0,0 +1,12 @@
+public class MathConstants {
+    double pi=3.14;
+    double e=2.14;
+    public void printvalue(){
+        System.out.println(pi);
+        System.out.println(e);
+    }
+
+    public void areaofCircle(int r) {
+        System.out.println(pi*r*r);
+    }
+}
