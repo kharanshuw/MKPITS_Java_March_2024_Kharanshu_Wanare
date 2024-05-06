32170060e566e8bffaadd9cc99a308da18142ee8
Kharanshu <kharanshuw@gmail>
2024-05-06 17:47:57 +0530

    assignment

---
+++ Java/Java_program_on_matrix/3.java
@@ -0,0 +1,28 @@
+ 
+ public class Matrix {
+ public void uppermatrix()
+    {
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (j>i)
+                {
+                    System.out.print(arr[i][j]+" ");
+                }
+                else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+	
+ }
