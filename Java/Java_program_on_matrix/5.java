2880f85c38f69a66217a428c194f490b85059212
Kharanshu <kharanshuw@gmail>
2024-05-07 14:46:13 +0530

    5

---
+++ Java/Java_program_on_matrix/5.java
@@ -0,0 +1,13 @@
+
+
+class Matrix{
+public void Transpose(int arr[][]) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				System.out.print(arr[j][i]+" ");
+			}
+			System.out.println("");
+		}
+	}	
+	
+	}
