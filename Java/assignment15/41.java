c3d836c99b3ef574edea76cc14e3b1cbf7a88ad6
Kharanshu Wanare <kharanshuw@gmail.com>
2024-05-27 15:09:58 +0530

    assignment

---
+++ Java/assignment15/41.java
@@ -0,0 +1,28 @@
+import java.util.Collections;
+import java.util.LinkedList;
+
+import java.util.ListIterator;
+
+public class Mylinkedlist {
+public static void main(String[] args) {
+	  LinkedList<Integer> linkedList=new LinkedList<Integer>();
+	  
+	  linkedList.add(78);
+	  linkedList.add(1);
+	  linkedList.add(1);
+	  linkedList.add(2);
+	  linkedList.add(1);
+	  linkedList.add(3);
+	  linkedList.add(1);
+	  linkedList.add(1);	  
+	
+	 
+	 
+	  
+	  System.out.println(linkedList.poll());
+	  
+	  System.out.println(linkedList);
+	  
+	  
+}
+}
