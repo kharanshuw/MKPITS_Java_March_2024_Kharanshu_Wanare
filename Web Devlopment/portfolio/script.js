7c9124226750c696071082e4d081a6b44c717c77
Kharanshu Wanare <kharanshuw@gmail.com>
2024-07-08 01:08:38 +0530

    portfolio

--- Web Devlopment/portfolio/script.js
+++ Web Devlopment/portfolio/script.js
@@ -101,3 +101,17 @@ window.addEventListener('scroll', updateActiveSection);
 updateActiveSection();
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr=ScrollReveal({
+  origin:'top',
+  distance:'60px',
+  duration:2500,
+  delay:400,
+})
+
+sr.reveal(`.home__data,.experience,.skills,.contact__container`)
+sr.reveal(`.home__img`,{delay :600})
+sr.reveal(`.home__scroll`,{delay :800})
+sr.reveal(`.work__card,.services__card`,{interval:800})
+sr.reveal(`.about__content`,{origin:'right'})
+sr.reveal(`.about__img`,{origin:'left'})
