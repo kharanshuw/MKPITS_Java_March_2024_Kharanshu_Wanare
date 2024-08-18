3ae501854cb5f166f298459ebed1b468181a1051
Kharanshu Wanare <kharanshuw@gmail.com>
2024-08-18 23:51:28 +0530

    OnlineShoppingCart

---
+++ Java/OnlineShoppingCart/ShoppingCART.java
@@ -0,0 +1,15 @@
+package onlineshoppingcart;
+
+public class ShoppingCART {
+	public void addtoCart(Product product) {
+		
+	}
+	
+	public void removeFromCart(Product product) {
+		
+	}
+	
+	public int TotalPrice() {
+		return 000;
+	}
+}
